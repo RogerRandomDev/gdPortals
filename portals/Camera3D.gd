719714f6e7a3b19543290014b8f43cb20f5634b0
Roger <rgrang816@west-mec.org>
2022-05-06 17:14:49 -0700

    portals render right now, time to make interactive

--- portals/Camera3D.gd
+++ portals/Camera3D.gd
@@ -7,11 +7,32 @@ func _ready():
 
 
 func _process(delta):
+	var dir=Vector2.ZERO
 	if Input.is_key_pressed(KEY_D):
-		transform.origin.x+=1*delta
+		dir.x+=1*delta
 	if Input.is_key_pressed(KEY_A):
-		transform.origin.x-=1*delta
+		dir.x-=1*delta
 	if Input.is_key_pressed(KEY_S):
-		transform.origin.z+=1*delta
+		dir.y+=1*delta
 	if Input.is_key_pressed(KEY_W):
-		transform.origin.z-=1*delta
+		dir.y-=1*delta
+	dir=dir.rotated(-rotation.y)
+	transform.origin+=Vector3(dir.x,0,dir.y)
+
+var in_game=true
+var sensitivity=0.01;
+var maxRotation=1.5708
+func _input(event):
+	#exits to menu
+	if Input.is_action_just_pressed("esc"):
+		in_game=!in_game
+		if in_game:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if event is InputEventMouseMotion&&in_game:
+		var rotBase=(Vector2(512,300)-get_viewport().get_mouse_position())*sensitivity
+		#rotates parent around x, but looking up and down is relative to camera
+		rotation.y+=rotBase.x
+		rotation.x=clamp(rotation.x+rotBase.y,-maxRotation,maxRotation)
+		get_viewport().warp_mouse(Vector2(512,300))
--- portals/portal.gd
+++ portals/portal.gd
@@ -1,15 +1,34 @@
 extends Node3D
 
 var updated=false
+@export_node_path(Node3D) var connectedPortal
 func _ready():
-	$MeshInstance3D.texture=$SubViewport.get_texture()
+	$MeshInstance3D.material_override.set_shader_param("TEXTURE",get_node(connectedPortal).get_portal_texture())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$SubViewport/Node3D.global_transform.origin=global_transform.origin
+#	var connectedPortalDat=get_node(connectedPortal).get_camera_info(rotation)
+	#this moves the camera where it is needed
+	var linked: Node = get_node(connectedPortal)
+	var trans: Transform3D = linked.global_transform.inverse() \
+			* get_viewport().get_camera_3d().global_transform
+	var up := Vector3(0, 1, 0)
+	trans = trans.rotated(up, PI)
+	get_node(connectedPortal).get_node("SubViewport/Node3D/Camera3D").transform = trans
+	var cam_pos: Transform3D = get_node(connectedPortal).get_node("SubViewport/Node3D/Camera3D").global_transform
+	
+
+
+#builds the information of where to move the connected portals camera
+func get_camera_info(rotval):
 	var gap=(Global.player.global_transform.origin-global_transform.origin)
-	var rotation_difference=(Global.player.rotation-rotation)
-	var distance=gap.length()
-	$SubViewport/Node3D/Camera3D.near=distance+0.01
-	$SubViewport/Node3D/Camera3D.transform.origin=-gap
-	$SubViewport/Node3D/Camera3D.rotation=-rotation_difference+Vector3(0,PI,0)
+	var rotation_difference=( -Global.player.rotation-rotation-rotval)
+	return [gap,rotation_difference]
+	
+
+
+#returns portal viewport
+func get_portal_texture():
+	return $SubViewport.get_texture()
+
+
